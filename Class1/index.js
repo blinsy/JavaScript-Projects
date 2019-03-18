bff991d70f240d6ed138dae80b7e1fec07eed93e
blinsy <maurynblinsy@gmail.com>
2019-03-18 13:06:18 +0300

    first commit

---
+++ Class1/index.js
@@ -0,0 +1,12 @@
+
+        //This is my first javascript code 
+        console.log('Hello world');
+
+
+      //   let name= 'maureen'; //string literal
+      //   let age=30; //number literal
+      //   let isApproved = false; //boolean
+      //   let firstName = undefined;
+
+      // let lastName= null; 
+        
